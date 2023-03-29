2fdafd4f7deaf2bf2834a0be3c53d71a994b44a1
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-03-29 20:13:50 +0200

    [Add] custom properties (zzIdBox)

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,22 @@
 const container = document.querySelector(".container");
+const btnReset = document.getElementById("resetButton");
+
+const arrayBox = [];
 
 //Ciclo per generare i box
 for (let i = 0 ; i < 100 ; i++) {
 
   const singleBox = generateBox();
+  //Ad ogni box generato assegno una custom properties che corrisponde al suo numero in tabella. il + 1 serve perchè l'indice parte da 0, mentre a me serve che la prima casella abbia i = 0 e l'ultima i=100
+  singleBox.zzIdBox = i + 1
 
   singleBox.addEventListener("click", function(){
     this.classList.toggle("active")
-    console.log(this)
+    console.log(this.zzIdBox)
+    singleBox.innerHTML = `${this.zzIdBox}`
   })
   container.appendChild(singleBox)
+  arrayBox.push(singleBox)
 
 }
 
@@ -24,4 +31,3 @@ function generateBox() {
 
   return newBox;
 }
-
